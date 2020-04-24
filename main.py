c72aa129eb3b3b34d250ea7bce614c38184d9894
Sachin <sachin_sathwara@yahoo.com>
2020-04-24 12:12:19 +0100

    New comment added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!usr/bin/python3
 
 def main (msg)
-    // Another comment  noumber
-    //Adding the second comment to this file.\
-    // Print a message
+    //Another comment  noumber
+    //adding the second comment to this file.\
+    //Print a message
     print(msg)
 
 main("Hello People")
